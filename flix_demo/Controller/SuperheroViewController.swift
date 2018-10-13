44dc133aecec8a5865612c60e535d92e752e93d2
roden777 <rhoden23@gmail.com>
2018-10-13 15:28:11 -0700

    added collection view

---
+++ flix_demo/Controller/SuperheroViewController.swift
@@ -0,0 +1,101 @@
+//
+//  SuperheroViewController.swift
+//  flix_demo
+//
+//  Created by Roden Susan on 10/13/18.
+//  Copyright © 2018 Roden. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+
+
+class SuperheroViewController: UIViewController , UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        fetchMovies()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+        let movie = movies[indexPath.item]
+        if let posterPathString = movie["poster_path"] as? String{
+            let baseURL = "https://image.tmdb.org/t/p/w500"
+            let posterURL = URL(string: baseURL + posterPathString)!
+            cell.posterImageView.af_setImage(withURL: posterURL)
+            
+        }
+        return cell
+    }
+    
+    func fetchMovies(){
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+                let alertController = UIAlertController(title: "Cannot Get Movies", message: "The internet connection appears to be offline", preferredStyle: .alert)
+                self.present(alertController, animated: true, completion: nil)
+                let ok = UIAlertAction(title: "Try Again", style: .default, handler: { (action) -> Void in })
+                alertController.addAction(ok)
+                
+                
+            } else if let data = data {
+                // Start the activity indicator
+                //.activityIndicator.startAnimating()
+                
+                // Stop the activity indicator
+                // Hides automatically if "Hides When Stopped" is enabled
+                //self.activityIndicator.stopAnimating()
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                let movies = dataDictionary["results"] as![[String: Any]]
+                //                for movie in movies{
+                //                    let title = movie["title"] as! String
+                //                    print(title)
+                //                }
+                self.movies = movies
+                
+                self.collectionView.reloadData()
+                //self.refreshControl.endRefreshing()
+                
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+                
+                
+                
+            }
+        }
+        task.resume()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ flix_demo/View/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flix_demo
+//
+//  Created by Roden Susan on 10/13/18.
+//  Copyright © 2018 Roden. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
